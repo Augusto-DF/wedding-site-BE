51aa548817fe99808fe3010a5a741dc0d80c04d2
Augusto-DF <neoaugustocode@gmail.com>
2024-06-24 17:21:29 -0300

    feat: Add GIFT_IMAGES_DIR path to utils

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
 const ROOT = path.join(__dirname);
+const GIFT_IMAGES_DIR = path.join(__dirname, "/assets/gifts_images");
 
-module.exports = { ROOT };
+module.exports = { ROOT, GIFT_IMAGES_DIR };
