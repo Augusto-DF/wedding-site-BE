cd37c119e17b177d38d9836b368ad1c65041bc75
Augusto-DF <neoaugustocode@gmail.com>
2024-06-24 18:58:16 -0300

    feat: Add gifts router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const giftListRouter = require("./giftsRoute");
 
 router.use("/", (req, res, next) => {
   console.log("On root");
   next();
 });
 
+router.use("/gifts", giftListRouter);
 module.exports = router;
