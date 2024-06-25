d3ba220fe32d6f6bac1fed7599ad8b43967e2613
Augusto-DF <neoaugustocode@gmail.com>
2024-06-25 20:04:13 -0300

    improve app file

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const routes = require("./routes/index");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
