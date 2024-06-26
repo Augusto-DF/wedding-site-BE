8ee18cf8cd62d4f06d6a803f82c2186e5f5df9e1
Augusto-DF <neoaugustocode@gmail.com>
2024-06-26 09:29:08 -0300

    feat: Add validateEmail to utils

--- utils.js
+++ utils.js
@@ -3,4 +3,9 @@ const path = require("path");
 const ROOT = path.join(__dirname);
 const GIFT_IMAGES_DIR = path.join(__dirname, "/assets/gifts_images");
 
-module.exports = { ROOT, GIFT_IMAGES_DIR };
+const validateEmail = (email) => {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+};
+
+module.exports = { ROOT, GIFT_IMAGES_DIR, validateEmail };
