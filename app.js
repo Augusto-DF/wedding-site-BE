9e00d9c388f35b845a403f931a50e83d3b92f817
Augusto-DF <neoaugustocode@gmail.com>
2024-07-02 12:47:15 -0300

    add doent lib

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const routes = require("./routes/index");
 
 const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening on http://localhost:8080`);
 });
